bb52651acf3af1dd11645415c87fb1fbae85207e
Hugo Bazille <hugo.bazille@gmail.com>
2024-01-08 12:18:02 +0100

    changed an import d17

--- 2023/day17p2.py
+++ 2023/day17p2.py
@@ -1,59 +1,5 @@
-class Heap:
-    def __init__(self, sizeMax):
-        self.heap = [None]
-        self.index = [-1]*sizeMax        
-    
-    def is_empty(self):
-        return len(self.heap) == 1
 
-    def _moveUp(self, pos):
-        (val, elt) = self.heap[pos]
-        while (pos > 1) and (val < self.heap[pos//2][0]):
-            self.heap[pos] = self.heap[pos//2]
-            self.index[self.heap[pos][1]] = pos
-            pos = pos // 2
-        self.heap[pos] = (val, elt)
-        self.index[elt] = pos
-                
-    def push(self, elt, val):
-        self.heap.append((val, elt))
-        self.index[elt] = len(self.heap)-1
-        self._moveUp(len(self.heap)-1)
-    
-    def update(self, elt, newVal):
-        pos = self.index[elt]
-        if pos == -1:
-            self.heap.append((newVal, elt))
-            pos = len(self.heap)-1
-            self.index[elt] = pos
-        else:
-            self.heap[pos] = (newVal, elt)
-        self._moveUp(pos)
-        
-    def pop(self):
-        e = self.heap[1]
-        self.index[e[1]] = -1
-        (val, elt) = self.heap[len(self.heap)-1]
-        self.heap.pop()
-        if not self.is_empty():
-            n = len(self.heap)-1
-            ok = False
-            i = 1    
-            while (i <= n // 2) and not ok:
-                j = 2 * i
-                if (j + 1 <= n) and (self.heap[j+1][0] < self.heap[j][0]):
-                    j = j + 1
-                if val > self.heap[j][0]:
-                    self.heap[i] = self.heap[j]
-                    self.index[self.heap[i][1]] = i
-                    i = j
-                else:
-                    ok = True
-            self.heap[i] = (val, elt)
-            self.index[self.heap[i][1]] = i
-            
-        return e
-
+from utils import heap
 
 def convert(i, j, s, c, n):
     return i * n + j + n*n*(4*c+s)
@@ -82,7 +28,7 @@ def create(h, d, i, j, s, c, v, n):
     d[i][j][s][c] = v
 
 def shortest(t, n):
-    h = Heap(n * n * 44)
+    h = heap.Heap(n * n * 44)
     d = []
     for i in range(n):
         d.append([])
